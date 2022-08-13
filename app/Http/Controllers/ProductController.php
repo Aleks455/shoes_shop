ecb4bedf9e56a28e135e83863dd8abce6fe8b9cc
Aleksandra555 <alekspetrovic.it@gmail.com>
2022-08-14 01:48:47 +0200

    Added README and deleted some testing data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,38 +7,8 @@ use Illuminate\Http\Request;
 
 class ProductController extends MainController
 {
-    // const PRODUCTS = [
-    //     [
-    //         'name' => 'Sneakers',
-    //         'description' => 'Fensy white sneakers',
-    //         'price' => '20',
-    //     ],
-    //     [
-    //         'name' => 'Shorts',
-    //         'description' => 'Fensy white shorts',
-    //         'price' => '25',
-    //     ],
-    //     [
-    //         'name' => 'Hat',
-    //         'description' => 'Oldfasion cilindar hat',
-    //         'price' => '14',
-    //     ],
-    //     [
-    //         'name' => 'Jeans',
-    //         'description' => 'Short purple jeans',
-    //         'price' => '30',
-    //     ],
-    //     [
-    //         'name' => 'Watch',
-    //         'description' => 'Cristal watch',
-    //         'price' => '180',
-    //     ],
-    // ];
-
     public function index()
     {
-        // $this->data['products'] = $this::PRODUCTS;
-
         $products = new Product();
         $result = $products->selectProducts();
         $this->data['products'] = $result;
